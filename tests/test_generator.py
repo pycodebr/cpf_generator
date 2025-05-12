fa2c56c8329827c17608c8418822caa5ef60b088
pycodebr <pycodebr@gmail.com>
2025-05-12 20:59:20 -0300

    first commit

---
+++ tests/test_generator.py
@@ -0,0 +1,14 @@
+from cpf.generator import generate_cpf
+from cpf.validator import validate_cpf
+import re
+
+def test_generate_cpf_formatted():
+    cpf = generate_cpf(formatted=True)
+    assert re.match(r'^\d{3}\.\d{3}\.\d{3}-\d{2}$', cpf)
+    assert validate_cpf(cpf)
+
+def test_generate_cpf_unformatted():
+    cpf = generate_cpf(formatted=False)
+    assert cpf.isdigit()
+    assert len(cpf) == 11
+    assert validate_cpf(cpf)
