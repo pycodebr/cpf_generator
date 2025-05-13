d7786f9bb38e517b1ba856029e14e4211a643232
pycodebr <pycodebr@gmail.com>
2025-05-12 21:34:20 -0300

    refeactoring

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -1,11 +1,14 @@
+import re
+
 from cpf.generator import generate_cpf
 from cpf.validator import validate_cpf
-import re
+
 
 def test_generate_cpf_formatted():
     cpf = generate_cpf(formatted=True)
     assert re.match(r'^\d{3}\.\d{3}\.\d{3}-\d{2}$', cpf)
     assert validate_cpf(cpf)
+
 
 def test_generate_cpf_unformatted():
     cpf = generate_cpf(formatted=False)
